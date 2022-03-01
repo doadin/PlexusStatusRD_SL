4172df6faad2405598c8a98f8acb2b28ee9fcb06
tbkizle <tbkizle@gmail.com>
2022-02-28 22:19:44 -0500

    Add Initial Support for Sepulcher of the First Ones

---
+++ SepulcheroftheFirstOnes.lua
@@ -0,0 +1,130 @@
+-- local zone = "Sepulcher of the First Ones"
+local zoneid = 2047
+--zoneid, first, second, icon_priority, color_priority, timer, stackable, color, default_disable, noicon
+
+-- Trash
+
+
+-- Vigilant Guardian
+GridStatusRaidDebuff:BossNameId(zoneid, 100, "Vigilant Guardian")
+GridStatusRaidDebuff:DebuffId(zoneid, 364447, 101, 5, 5, true) -- Dissonance
+GridStatusRaidDebuff:DebuffId(zoneid, 360202, 102, 5, 5, true) -- Spike of Creation
+GridStatusRaidDebuff:DebuffId(zoneid, 367571, 103, 5, 5, true) -- Sear
+GridStatusRaidDebuff:DebuffId(zoneid, 360458, 104, 5, 5, true) -- Unstable Core
+GridStatusRaidDebuff:DebuffId(zoneid, 364881, 105, 5, 5, true) -- Matter Dissolution
+GridStatusRaidDebuff:DebuffId(zoneid, 364904, 106, 4, 4, true) -- anti-matter
+GridStatusRaidDebuff:DebuffId(zoneid, 360414, 107, 4, 4, true) -- Pneumatic Impact
+GridStatusRaidDebuff:DebuffId(zoneid, 364843, 108, 4, 4, true) -- Fractured Core
+GridStatusRaidDebuff:DebuffId(zoneid, 364962, 109, 4, 4, true) -- Core Overload
+
+-- Skolex, the Insatiable Ravener
+GridStatusRaidDebuff:BossNameId(zoneid, 200, "Skolex, the Insatiable Ravener")
+GridStatusRaidDebuff:DebuffId(zoneid, 359778, 201, 6, 6) -- Ephemera Dust
+GridStatusRaidDebuff:DebuffId(zoneid, 360098, 202, 6, 6) -- Warp Sickness
+GridStatusRaidDebuff:DebuffId(zoneid, 366070, 203, 4, 4) -- Volatile Residue
+GridStatusRaidDebuff:DebuffId(zoneid, 364522, 204, 6, 6, true, true) -- Devouring Blood
+GridStatusRaidDebuff:DebuffId(zoneid, 359976, 205, 5, 5, true, true) -- Riftmaw
+
+-- Artificer Xy'mox
+GridStatusRaidDebuff:BossNameId(zoneid, 300, "Artificer Xy'mox")
+GridStatusRaidDebuff:DebuffId(zoneid, 363413, 301, 5, 5, true, true) -- Genesis Rings
+GridStatusRaidDebuff:DebuffId(zoneid, 365752, 302, 5, 5) -- Ancient Exhaust
+GridStatusRaidDebuff:DebuffId(zoneid, 365801, 303, 5, 5, true, true) -- Relic Collapse
+GridStatusRaidDebuff:DebuffId(zoneid, 363114, 304, 5, 5) -- Genesis Supernova
+GridStatusRaidDebuff:DebuffId(zoneid, 365682, 305, 5, 5, true, true) -- Massive Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 364030, 306, 5, 5) -- Debilitating Ray
+
+-- Dausegne, the Fallen Oracle
+GridStatusRaidDebuff:BossNameId(zoneid, 400, "Dausegne, the Fallen Oracle")
+GridStatusRaidDebuff:DebuffId(zoneid, 361966, 401, 5, 5, true) -- Infused Strikes
+GridStatusRaidDebuff:DebuffId(zoneid, 361751, 402, 5, 5, true) -- Disintegration Halo
+
+-- Prototype Pantheon
+GridStatusRaidDebuff:BossNameId(zoneid, 500, "Prototype Pantheon")
+GridStatusRaidDebuff:DebuffId(zoneid, 360687, 501, 4, 4, true, true) -- Runecarvers Deathtouch
+GridStatusRaidDebuff:DebuffId(zoneid, 360259, 502, 5, 5, true, true) -- Gloom Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 365041, 503, 5, 5) -- Windswept  Wings
+GridStatusRaidDebuff:DebuffId(zoneid, 362352, 504, 5, 5, true, true) -- Pinned
+GridStatusRaidDebuff:DebuffId(zoneid, 361784, 505, 5, 5) -- Touch of the Night
+GridStatusRaidDebuff:DebuffId(zoneid, 362383, 506, 5, 5, true, true) -- Anima Bolt
+GridStatusRaidDebuff:DebuffId(zoneid, 361689, 507, 4, 4, true, true) -- Wracking Pain
+GridStatusRaidDebuff:DebuffId(zoneid, 361608, 508, 5, 5, true, true) -- Urden of Sin
+GridStatusRaidDebuff:DebuffId(zoneid, 364867, 509, 5, 5) -- Sinful Projection
+
+-- Lihuvim, Principal Architect
+GridStatusRaidDebuff:BossNameId(zoneid, 600, "Lihuvim, Principal Architect")
+GridStatusRaidDebuff:DebuffId(zoneid, 360159, 601, 5, 5, true) -- Unstable Mote
+GridStatusRaidDebuff:DebuffId(zoneid, 363681, 602, 5, 5, true, true) -- Deconstructing Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 368024, 603, 5, 5) -- Kinetic Resonance
+GridStatusRaidDebuff:DebuffId(zoneid, 368025, 604, 5, 5) -- Sundering Resonance
+GridStatusRaidDebuff:DebuffId(zoneid, 364092, 605, 5, 5) -- Degenerate
+GridStatusRaidDebuff:DebuffId(zoneid, 366012, 606, 5, 5, true, true) -- Terminal Mote
+GridStatusRaidDebuff:DebuffId(zoneid, 362447, 607, 4, 4, true, true) -- Instability
+
+-- Halondrus the Reclaimer
+GridStatusRaidDebuff:BossNameId(zoneid, 700, "Halondrus the Reclaimer")
+GridStatusRaidDebuff:DebuffId(zoneid, 368961, 701, 5, 5, true) -- Charge Exposure
+GridStatusRaidDebuff:DebuffId(zoneid, 360114, 702, 5, 5, true) -- Ephemeral Fissure
+GridStatusRaidDebuff:DebuffId(zoneid, 361597, 703, 5, 5, true) -- Ephemeral Eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 368529, 704, 5, 5) -- Eternity Overdrive
+
+-- Anduin Wrynn
+GridStatusRaidDebuff:BossNameId(zoneid, 800, "Anduin Wrynn")
+GridStatusRaidDebuff:DebuffId(zoneid, 368913, 801, 5, 5, true) -- Force of Will
+GridStatusRaidDebuff:DebuffId(zoneid, 362055, 802, 5, 5, true) -- Lost Soul
+GridStatusRaidDebuff:DebuffId(zoneid, 365445, 803, 5, 5) -- Scarred Soul
+GridStatusRaidDebuff:DebuffId(zoneid, 364031, 804, 5, 5, true) -- Gloom
+GridStatusRaidDebuff:DebuffId(zoneid, 365293, 805, 5, 5, true) -- Befouled Barrier
+GridStatusRaidDebuff:DebuffId(zoneid, 366849, 806, 5, 5, true) -- Domination Word Pain
+GridStatusRaidDebuff:DebuffId(zoneid, 363024, 807, 5, 5) -- Necrotic Detonation
+GridStatusRaidDebuff:DebuffId(zoneid, 363020, 808, 5, 5) -- Necrotic Claws
+GridStatusRaidDebuff:DebuffId(zoneid, 364020, 809, 5, 5) -- March of the Damned
+GridStatusRaidDebuff:DebuffId(zoneid, 362543, 810, 5, 5) -- Remorseless Winter
+GridStatusRaidDebuff:DebuffId(zoneid, 363028, 811, 5, 5) -- Unraveling Frenzy
+GridStatusRaidDebuff:DebuffId(zoneid, 368428, 812, 5, 5) -- Purging Light
+
+-- Lords of Dread
+GridStatusRaidDebuff:BossNameId(zoneid, 900, "Lords of Dread")
+GridStatusRaidDebuff:DebuffId(zoneid, 360300, 901, 5, 5, true) -- Swarm of Decay
+GridStatusRaidDebuff:DebuffId(zoneid, 360304, 902, 5, 5, true) -- Swarm of Darkness
+GridStatusRaidDebuff:DebuffId(zoneid, 362158, 903, 5, 5, true) -- Decay Mastery
+GridStatusRaidDebuff:DebuffId(zoneid, 364985, 904, 5, 5, true, true) -- Biting Wounds
+GridStatusRaidDebuff:DebuffId(zoneid, 360008, 905, 5, 5, nil, true) -- Cloud of Carrion
+GridStatusRaidDebuff:DebuffId(zoneid, 359963, 906, 5, 5) -- Opened Veins
+GridStatusRaidDebuff:DebuffId(zoneid, 360418, 907, 5, 5, true) -- Paranoia
+GridStatusRaidDebuff:DebuffId(zoneid, 360420, 908, 5, 5) -- Shatter Mind
+GridStatusRaidDebuff:DebuffId(zoneid, 362152, 909, 5, 5) -- Shadow Mastery
+GridStatusRaidDebuff:DebuffId(zoneid, 360148, 910, 5, 5) -- Bursting Dread
+GridStatusRaidDebuff:DebuffId(zoneid, 360241, 911, 5, 5) -- Unsettling Dreams
+
+-- Rygelon
+GridStatusRaidDebuff:BossNameId(zoneid, 1000, "Rygelon")
+GridStatusRaidDebuff:DebuffId(zoneid, 361548, 1001, 5, 5, true, true) -- Dark Eclipse
+GridStatusRaidDebuff:DebuffId(zoneid, 362273, 1002, 5, 5, true, true) -- Quasar Radiation
+GridStatusRaidDebuff:DebuffId(zoneid, 362206, 1003, 5, 5, true, true) -- Event Horizon
+GridStatusRaidDebuff:DebuffId(zoneid, 362081, 1004, 5, 5, nil, true) -- Cosmic Ejection
+GridStatusRaidDebuff:DebuffId(zoneid, 362088, 1005, 5, 5) -- Cosmic Irregularity
+GridStatusRaidDebuff:DebuffId(zoneid, 366379, 1006, 5, 5, true) -- Stellar Shroud
+GridStatusRaidDebuff:DebuffId(zoneid, 362172, 1007, 5, 5, true) -- Corrupted Wound
+GridStatusRaidDebuff:DebuffId(zoneid, 363598, 1008, 5, 5, true) -- Unstable Antimatter
+GridStatusRaidDebuff:DebuffId(zoneid, 364386, 1009, 5, 5, true) -- Gravitational Collapse
+
+-- The Jailer
+GridStatusRaidDebuff:BossNameId(zoneid, 1100, "The Jailer")
+GridStatusRaidDebuff:DebuffId(zoneid, 366381, 1101, 5, 5, true, true) -- Arcane Vulnerability
+GridStatusRaidDebuff:DebuffId(zoneid, 366703, 1102, 5, 5, true, true) -- Azerite Radiation
+GridStatusRaidDebuff:DebuffId(zoneid, 362075, 1103, 5, 5, true, true) -- Domination
+GridStatusRaidDebuff:DebuffId(zoneid, 362631, 1104, 5, 5, nil, true) -- Chains of Oppression
+GridStatusRaidDebuff:DebuffId(zoneid, 363893, 1105, 5, 5) -- Martyrdom
+GridStatusRaidDebuff:DebuffId(zoneid, 363886, 1106, 5, 5, true) -- Imprisonment
+GridStatusRaidDebuff:DebuffId(zoneid, 362194, 1107, 5, 5, true) -- Suffering
+GridStatusRaidDebuff:DebuffId(zoneid, 362192, 1108, 5, 5, true) -- Relentless Misery
+GridStatusRaidDebuff:DebuffId(zoneid, 366665, 1109, 5, 5, true) -- Unholy Eruption
+GridStatusRaidDebuff:DebuffId(zoneid, 359868, 1110, 5, 5, true) -- Shattering Blast
+GridStatusRaidDebuff:DebuffId(zoneid, 366777, 1111, 5, 5, true) -- Consumed Azerite
+GridStatusRaidDebuff:DebuffId(zoneid, 360425, 1112, 5, 5, true) -- Unholy Ground
+GridStatusRaidDebuff:DebuffId(zoneid, 366285, 1113, 5, 5, true) -- Rune of Compulsion
+GridStatusRaidDebuff:DebuffId(zoneid, 365153, 1114, 5, 5, true) -- Dominating Will
+GridStatusRaidDebuff:DebuffId(zoneid, 365219, 1115, 5, 5, true) -- Chains of Anguish
+GridStatusRaidDebuff:DebuffId(zoneid, 365385, 1116, 5, 5, true) -- Cry of Loathing
+GridStatusRaidDebuff:DebuffId(zoneid, 365174, 1117, 5, 5, true) -- Defile
